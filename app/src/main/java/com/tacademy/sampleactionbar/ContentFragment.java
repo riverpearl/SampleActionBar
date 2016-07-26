45fd4fedcd6c5a0893de43eef2c7d4cd33286ae1
River Pearl <riverpearl0115@gmail.com>
2016-07-26 15:29:02 +0900

    Initial commit

---
+++ app/src/main/java/com/tacademy/sampleactionbar/ContentFragment.java
@@ -0,0 +1,59 @@
+package com.tacademy.sampleactionbar;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ContentFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_MESSAGE = "param1";
+
+    // TODO: Rename and change types of parameters
+    private String message;
+
+    public ContentFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static ContentFragment newInstance(String param1) {
+        ContentFragment fragment = new ContentFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_MESSAGE, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            message = getArguments().getString(ARG_MESSAGE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+        TextView messageView = (TextView) view.findViewById(R.id.text_message);
+        messageView.setText(message);
+
+        Button btn = (Button) view.findViewById(R.id.btn_next);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((OtherActivity) getActivity()).showNext();
+            }
+        });
+
+        return view;
+    }
+}
